d8aaa95d036da351f3dd1a719565633da5b4b4ae
TashaBolsh <SerdcevaTanyaN@gmail.ru>
2023-08-28 16:05:32 +0300

    add soc_set picture

--- src/components/Profile/MyPosts/Post/Post.tsx
+++ src/components/Profile/MyPosts/Post/Post.tsx
@@ -8,7 +8,7 @@ type PostPropsType = {
 export const Post = (props:PostPropsType) => {
     return (
         <div className={classes.item}>
-            <img src="https://i.pinimg.com/originals/f5/27/41/f52741fb62bf1d821948a49204406bdc.jpg"/>
+            <img src="https://img.freepik.com/free-photo/beauty-fashion-portrait-of-young-blond-woman-model-with-natural-makeup-and-perfect-skin-posing_158538-8776.jpg?w=740&t=st=1693227174~exp=1693227774~hmac=200249cb2ecb1281f7eca5f3eedc23782e9add1a0c580a9a65ee5f25e525a38e"/>
             {props.message}
             <div>
                 <span>like</span>
